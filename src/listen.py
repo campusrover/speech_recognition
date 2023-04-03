8789f6b3c09b579df60f05972ba8d7ecdc68f4af
vbelkina <vbelkina95@gmail.com>
2023-04-03 13:35:57 -0400

    added self.device_index

--- src/listen.py
+++ src/listen.py
@@ -17,14 +17,14 @@ class Listen():
         self.r = sr.Recognizer()
         self.r.dynamic_energy_threshold = False
         self.r.energy_threshold = 400
-
+        self.device_index = 10
         self.command_pub = rospy.Publisher("/whisper/command", String, queue_size=1)
         
     def calibrate_mic(self):
         """
         calibrate the mic with some ambient noise
         """
-        with sr.Microphone() as source:  
+        with sr.Microphone(device_index=self.device_index) as source:  
             rospy.loginfo(f"{self.node_name} Please wait. Calibrating microphone...")  
             # listen for 5 seconds and create the ambient noise energy level  
             self.r.adjust_for_ambient_noise(source, duration=5)  
@@ -32,7 +32,7 @@ class Listen():
     def listen(self):
         result = ""
 
-        with sr.Microphone() as source:
+        with sr.Microphone(device_index=self.device_index) as source:
             rospy.loginfo(f"{self.node_name} listening...")
             audio = self.r.listen(source)
 
