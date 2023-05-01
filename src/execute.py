81c8f445d84d8db1098748d8acd2afd6bccaea6f
vbelkina <vbelkina95@gmail.com>
2023-05-01 12:52:12 -0400

    minor

--- src/execute.py
+++ src/execute.py
@@ -94,13 +94,15 @@ class Execute():
                 # publish the command to the appropriate topic over the roslibpy client
                 pub = roslibpy.Topic(self.client, current_command["receiver"], current_command["type"])
 
-                if self.prev_command != self.command: 
-                    rospy.loginfo(f"{self.node_name} Publishing {command} to {current_command['receiver']} topic with type {current_command['type']}.")
-                    self.speak(f"Executing command {command}")
+
 
                 # self.prev_command = self.command
             
                 pub.publish(roslibpy.Message(current_command["msg"]))
+
+                if self.prev_command != self.command: 
+                    rospy.loginfo(f"{self.node_name} Publishing {command} to {current_command['receiver']} topic with type {current_command['type']}.")
+                    self.speak(f"Executing command {command}")
 
                 # pub.unadvertise()
                 
